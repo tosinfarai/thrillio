673b8b8c50f3aa778a33e83b344b7ab0928af233
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-02-27 16:56:18 +0100

    Update UserManager.java

--- UserManager.java
+++ UserManager.java
@@ -1,15 +1,24 @@
+/*
+ * Manages all users
+ */
+
 package com.semanticsquare.thrillio.managers;
 
 import com.semanticsquare.thrillio.entities.User;
 
 public class UserManager {
+	
+	//Creates one instance of the UserManager Class  
 	private static UserManager instance = new UserManager();
+	
 	private UserManager() {}
 	
+	//Allows the one instance of the UserManager to be accessed globally
 	public static UserManager getInstance() {
 		return instance;
 	}
 	
+	//invoke methods to set fields of the user object
 	public User createUser(long id, String email, String password, String firstName, String lastName, int gender,
 			String userType) {
 		User user = new User();
