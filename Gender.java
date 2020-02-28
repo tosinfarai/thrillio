58905782741fa2caf84308b97278b9c73db4a09c
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-02-28 12:32:42 +0100

    Add files via upload

---
+++ Gender.java
@@ -0,0 +1,10 @@
+package com.semanticsquare.thrillio.constants;
+
+public class Gender {
+	private Gender () {}
+	
+	public static final int MALE = 0;
+	public static final int FEMALE = 1;
+	public static final int TRANSGENDER = 2;
+	
+}
---
+++ UserType.java
@@ -0,0 +1,9 @@
+package com.semanticsquare.thrillio.constants;
+
+public class UserType {
+	private UserType () {}
+	
+	public static final String USER = "user";
+	public static final String EDITOR = "editor";
+	public static final String CHIEF_EDITOR = "chief editor";
+}
