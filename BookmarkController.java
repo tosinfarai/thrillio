2c6e43374dc16e74ff5007e52e97d14ecd392082
tosinfarai <53543135+tosinfarai@users.noreply.github.com>
2020-03-16 18:45:55 +0100

    Add files via upload

---
+++ BookmarkController.java
@@ -0,0 +1,25 @@
+package com.semanticsquare.thrillio.controllers;
+
+import com.semanticsquare.thrillio.entities.Bookmark;
+import com.semanticsquare.thrillio.entities.User;
+import com.semanticsquare.thrillio.managers.BookmarkManager;
+
+public class BookmarkController {
+	private static BookmarkController instance = new BookmarkController();
+	private BookmarkController() {}
+	public static BookmarkController getInstance() {
+		return instance;
+	}
+	public void saveUserBookmark(User user, Bookmark bookmark) {
+		BookmarkManager.getInstance().saveUserBookmark(user, bookmark);
+		
+	}
+	public void setKidFriendlyStatus(User user, String kidFriendlyStatus, Bookmark bookmark) {
+		BookmarkManager.getInstance().setKidFriendlyStatus(user, kidFriendlyStatus, bookmark);
+		
+	}
+	public void share(User user, Bookmark bookmark) {
+		BookmarkManager.getInstance().share(user, bookmark);
+		
+	}
+}
